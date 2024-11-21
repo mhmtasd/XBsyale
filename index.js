6793d37c43efa3fdaa38b0905289ddd83d59fe56
mhmtasd <162480356+mhmtasd@users.noreply.github.com>
2024-11-21 19:33:56 +0300

    Anchorette

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let botConnected = false; // Bot bağlantı durumu
 // Bot yapılandırma ayarları
 const config = {
   botAccount: {
-    username: "ToastWizard",
+    username: "TeaKettle",
     password: "fake3",
     type: "legacy"
   },
@@ -28,7 +28,7 @@ const config = {
         { text: "/register fake3 fake3", delay: 15 },        
         { text: "/is accept EymanBey", delay: 15 },
         { text: "/skyblock", delay: 5 },
-        { text: "/is go AdanaDurum", delay: 15 }
+        { text: "/is go Anchorette", delay: 15 }
       ]
     },
     antiAfk: {
