e84640535e8e49ea20e398f2390c34c2db6a5321
mhmtasd <162480356+mhmtasd@users.noreply.github.com>
2024-11-21 19:55:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mineflayer = require('mineflayer');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 1000;
 
 let botConnected = false; // Bot bağlantı durumu
 
